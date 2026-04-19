0b97001df74a839eb9099ec9ac1d4bf22e5bbe48
ds6161855 <DSMEWADA61@GMAIL.COM>
2026-04-19 22:23:08 +0530

    done

--- src/Api/axiosConfig.js
+++ src/Api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://easybuy-backend-xadk.onrender.com",
+  baseURL: "https://easybuy-backend-85si.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
